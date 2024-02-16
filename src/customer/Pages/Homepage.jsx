c3a5bcbfb03efd17b4afd08a2385049c0a051183
Ananta Vaishnavi Movva <movvavaishnavi2503@gmail.com>
2024-02-16 13:42:09 +0530

    created homepage

--- src/customer/Pages/Homepage.jsx
+++ src/customer/Pages/Homepage.jsx
@@ -6,7 +6,7 @@ export default function Homepage() {
 
         <div>
             <CarouselBanner />
-            <div className='space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10'>
+            <div className='space-y-10 py-20 flex flex-col justify-center px-5  lg:px-10'>
                 <HomeSectionCarousel />
                 <HomeSectionCarousel />
             </div>
--- src/customer/components/HomeSectionCarousel.jsx
+++ src/customer/components/HomeSectionCarousel.jsx
@@ -4,27 +4,23 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 import HomeSectionCard from './HomeSectionCard';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import { Button } from '@mui/material';
-const responsive = {
-    0: { items: 1 },
-    480: { items: 1.5 },
-    720: { items: 3 },
-    1024: { items: 5.5 }
-};
 
-const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
+const HomeSectionCarousel = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
 
-
-export default function HomeSectionCarousel() {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const slidePrev = () => {
-        const currentIndex = currentIndex - 1;
-        setCurrentIndex(currentIndex);
+    const responsive = {
+        0: { items: 1 },
+        480: { items: 1.5 },
+        720: { items: 3 },
+        1024: { items: 5.5 }
     };
-    const slideNext = () => {
-        const currentIndex = currentIndex + 1;
-        setCurrentIndex(currentIndex);
-    };
-    const syncActiveIndex = ({ item }) => setCurrentIndex(item);
+
+    const slidePrev = () => setActiveIndex(activeIndex - 1);
+    const slideNext = () => setActiveIndex(activeIndex + 1);
+
+    const syncActiveIndex = ({ item }) => setActiveIndex(item);
+    const items = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item, index) => <HomeSectionCard key={index} />);
+
     return (
         <div className="relative px-4 lg:px-6">
             <div className="relative p-5">
@@ -33,35 +29,44 @@ export default function HomeSectionCarousel() {
                     disableButtonsControls
                     disableDotsControls
                     infinite
+                    onSlideChange={syncActiveIndex}
                     responsive={responsive}
+                    activeIndex={activeIndex}
                 />
+
                 <Button
+                    onClick={slidePrev}
                     variant="contained"
-                    className="z-50"
-                    sx={{
-                        position: 'absolute',
-                        top: '8rem',
-                        right: '0rem',
-                        transform: 'translateX(50%) rotate(90deg)', bgcolor: "white"
-                    }}
-                    aria-label="next"
-                >
-                    <KeyboardArrowLeftIcon sx={{ transform: 'rotate(90deg)', color: "black" }} />
-                </Button>
-                <Button
-                    variant="contained"
-                    className="z-50"
                     sx={{
                         position: 'absolute',
                         top: '8rem',
                         left: '0rem',
-                        transform: 'translateX(-50%) rotate(-90deg)', bgcolor: "white"
+                        transform: 'translateX(-50%) rotate(-90deg)',
+                        bgcolor: 'white'
+                    }}
+                    aria-label="previous"
+                >
+                    <KeyboardArrowLeftIcon sx={{ transform: 'rotate(90deg)', color: 'black' }} />
+                </Button>
+
+                {activeIndex !== items.length - 5 && <Button
+                    onClick={slideNext}
+                    variant="contained"
+                    sx={{
+                        position: 'absolute',
+                        top: '8rem',
+                        right: '0rem',
+                        transform: 'translateX(50%) rotate(90deg)',
+                        bgcolor: 'white'
                     }}
                     aria-label="next"
                 >
-                    <KeyboardArrowLeftIcon sx={{ transform: 'rotate(90deg)', color: "black" }} />
+                    <KeyboardArrowLeftIcon sx={{ transform: 'rotate(90deg)', color: 'black' }} />
                 </Button>
+                }
             </div>
         </div>
     );
-}
+};
+
+export default HomeSectionCarousel;
