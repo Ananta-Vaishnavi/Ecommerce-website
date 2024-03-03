05ccb8fd19d31bd1c141df08785bd08f40144f93
Ananta Vaishnavi Movva <movvavaishnavi2503@gmail.com>
2024-03-03 15:10:27 +0530

    carousel implemented

--- src/customer/Pages/Homepage.jsx
+++ src/customer/Pages/Homepage.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
 import CarouselBanner from './CarouselBanner';
 import HomeSectionCarousel from '../components/HomeSectionCarousel';
+import { mens_kurta } from "../../Data/Men/men_kurta";
+
 export default function Homepage() {
     return (
-
         <div>
             <CarouselBanner />
-            <div className='space-y-10 py-20 flex flex-col justify-center px-5  lg:px-10'>
-                <HomeSectionCarousel />
-                <HomeSectionCarousel />
+            <div className='space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10'>
+                <HomeSectionCarousel data={mens_kurta} sectionName={"Men's Kurta"} />
+                <HomeSectionCarousel data={mens_kurta} sectionName={"Men's Shoes"} />
             </div>
         </div>
-    )
+    );
 }
